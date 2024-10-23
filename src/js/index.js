a7fd67244ce685edbc982e030cee807bd492d556
Mokhamed B <m.barhush@gmail.com>
2024-10-23 21:24:01 +0200

    animation not corrected

--- src/js/index.js
+++ src/js/index.js
@@ -33,6 +33,20 @@ window.addEventListener("load", function () {
   const video4 = new Plyr("#video-4", plyr_options);
   const video5 = new Plyr("#video-5", plyr_options);
 
+  // video1.on
+  // let a = document.querySelectorAll(".block_media");
+
+  video1.on("play", (event) => {
+    body.classList.add("js_active_video");
+    // a.forEach((el) => {
+    //   el.style.width = "1000px";
+    //   el.style.height = "1000px";
+    // });
+  });
+  video1.on("pause", (event) => {
+    body.classList.remove("js_active_video");
+  });
+
   // Показываем страницу после загрузки
   body.classList.remove("js_load");
   body.style.opacity = 1;
