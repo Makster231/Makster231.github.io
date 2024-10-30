45dbe757edecb915f3ec9fbb021dcd5b26fa8b8b
Mokhamed B <m.barhush@gmail.com>
2024-10-30 23:45:46 +0100

    added dinamic info to modal

--- src/js/modules/requestForm.js
+++ src/js/modules/requestForm.js
@@ -1,4 +1,5 @@
 const requestForm = function () {
+  const lvl_5_2 = document.querySelector(".js_lvl_5-2");
   const formWrapper = document.querySelector(".js_request");
 
   const btnOpen = document.querySelectorAll(".js_request_open");
@@ -6,26 +7,58 @@ const requestForm = function () {
 
   let isOpened = false;
 
-  // const btnOpen = document.querySelector(".js_request_open");
-  // const btnClose = document.querySelector(".js_request_close");
-
   btnOpen?.forEach((el) => {
-    el.addEventListener("click", () => handleClick("open"));
+    el.addEventListener("click", (e) => handleClick(e, "open"));
   });
   btnClose?.forEach((el) => {
-    el.addEventListener("click", () => handleClick("close"));
+    el.addEventListener("click", (e) => handleClick(e, "close"));
   });
 
-  // btnOpen?.addEventListener("click", () => handleClick("open"));
-  // btnClose?.addEventListener("click", () => handleClick("close"));
+  function handleClick(e, state) {
+    e.preventDefault();
+    e.stopPropagation();
 
-  function handleClick(state) {
     if (state === "close") {
       formWrapper.classList.remove("js_active");
 
       isOpened = false;
     } else if (state === "open") {
       formWrapper.classList.add("js_active");
+
+      // GET INFO
+      let getWrapper, getHref, getTitle, getText, getBg;
+
+      if (lvl_5_2.getAttribute("data-tab") == 4) {
+        getWrapper = document.querySelectorAll(".js_final_4_list-item")[
+          +lvl_5_2.getAttribute("data-bg") - 1
+        ];
+
+        getBg = document
+          .querySelectorAll(".final_4_bg")
+          [+lvl_5_2.getAttribute("data-bg") - 1].querySelector("img").srcset;
+      } else {
+        getWrapper = e.target.closest(".js_request_open");
+
+        getBg =
+          getWrapper.querySelector(".js_request_open-bg img")?.src ||
+          "./img/final_steps/blue.jpg";
+      }
+
+      getHref = getWrapper?.getAttribute("href");
+      getTitle = getWrapper.querySelector(".js_request_open-title")?.innerHTML;
+      getText = getWrapper.querySelector(".js_request_open-text")?.innerHTML;
+
+      // SET INFO
+      const setWrapper = document.querySelector(".js_request_link");
+      const setLink = setWrapper.querySelector(".js_request_link-href");
+      const setTitle = setWrapper.querySelector(".js_request_link-title");
+      const setText = setWrapper.querySelector(".js_request_link-text");
+      const setBg = setWrapper.querySelector(".js_request_link-bg img");
+
+      setLink.setAttribute("href", getHref);
+      setTitle.innerHTML = getTitle;
+      setText.innerHTML = getText;
+      setBg.src = getBg;
 
       isOpened = true;
     }
@@ -40,6 +73,7 @@ const requestForm = function () {
     if (!formWrapper.classList.contains("js_active")) return;
 
     if (
+      e.target.closest(".js_request_link") ||
       e.target.matches(".js_request_close") ||
       e.target.matches(".js_request_open") ||
       !isOpened
@@ -47,7 +81,7 @@ const requestForm = function () {
       return;
 
     if (!clickContains.contains(e.target)) {
-      handleClick("close");
+      handleClick(e, "close");
     }
   });
 
