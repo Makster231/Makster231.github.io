659b90d86155fcb9b825f99e678521d081ae7213
Mokhamed B <m.barhush@gmail.com>
2024-11-02 17:09:33 +0100

    corrected 0 step

--- src/js/index.js
+++ src/js/index.js
@@ -33,19 +33,26 @@ window.addEventListener("load", function () {
   const video4 = new Plyr("#video-4", plyr_options);
   const video5 = new Plyr("#video-5", plyr_options);
 
-  // video1.on
-  // let a = document.querySelectorAll(".block_media");
+  video1.on("play", (event) => {
+    body.classList.add("js_active_video");
+  });
+  video1.on("pause", (event) => {
+    body.classList.remove("js_active_video");
+  });
 
-  // video1.on("play", (event) => {
-  //   body.classList.add("js_active_video");
-  //   // a.forEach((el) => {
-  //   //   el.style.width = "1000px";
-  //   //   el.style.height = "1000px";
-  //   // });
-  // });
-  // video1.on("pause", (event) => {
-  //   body.classList.remove("js_active_video");
-  // });
+  function isFullScreen() {
+    return window.screenTop == 0 ? true : false;
+  }
+
+  document.addEventListener("fullscreenchange", function () {
+    if (!isFullScreen()) {
+      body.classList.add("js_load");
+    } else {
+      setInterval(() => {
+        body.classList.remove("js_load");
+      }, 20);
+    }
+  });
 
   // Показываем страницу после загрузки
   body.classList.remove("js_load");
