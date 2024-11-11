4551b96df96336709fb3de07d7da673cef7fcef0
Mokhamed B <m.barhush@gmail.com>
2024-11-11 17:50:34 +0100

    build

--- src/js/modules/video.js
+++ src/js/modules/video.js
@@ -4,23 +4,46 @@ const video = function () {
 
   var plyr_options = {
     showPosterOnEnd: true,
-    fullscreen: { enabled: isMobile ? false : true },
+    // fullscreen: { enabled: isMobile ? false : true },
+    fullscreen: { enabled: true },
     clickToPlay: true,
   };
 
   videos.forEach((vid) => {
     const video = new Plyr(vid, plyr_options);
     const poster = document.querySelectorAll(".plyr__poster");
+    const close =
+      video.elements.container.parentNode.querySelector(".close-video");
 
+    close.addEventListener("click", () => {
+      body.classList.remove("js_active_video");
+      poster.forEach((el) => {
+        el.style.opacity = 1;
+        close.classList.remove("close-video-active");
+      });
+
+      setTimeout(() => {
+        body.classList.remove("js_load");
+      }, 100);
+    });
     video.on("play", (e) => {
-      if (isMobile) body.classList.add("js_load");
+      if (isMobile) {
+        body.classList.add("js_load");
+        video.fullscreen.enter();
+      }
 
       body.classList.add("js_active_video");
-      poster.forEach((el) => (el.style.opacity = 0));
+      poster.forEach((el) => {
+        el.style.opacity = 0;
+        close.classList.add("close-video-active");
+      });
     });
     video.on("pause", (e) => {
       body.classList.remove("js_active_video");
-      poster.forEach((el) => (el.style.opacity = 1));
+      poster.forEach((el) => {
+        el.style.opacity = 1;
+        close.classList.remove("close-video-active");
+      });
 
       setTimeout(() => {
         body.classList.remove("js_load");
@@ -28,7 +51,10 @@ const video = function () {
     });
     video.on("end", (e) => {
       body.classList.remove("js_active_video");
-      poster.forEach((el) => (el.style.opacity = 1));
+      poster.forEach((el) => {
+        el.style.opacity = 1;
+        close.classList.remove("close-video-active");
+      });
 
       setTimeout(() => {
         body.classList.remove("js_load");
